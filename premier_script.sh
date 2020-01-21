bcda8a8f115a11769f01a24c1c76f9981b16f43a
art.dambrine <art.dambrine@gmail.com>
2020-01-22 00:44:31 +0100

    Test redirect >

--- premier_script.sh
+++ premier_script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo "hello world!"
+#echo "hello world!"
+echo "On va tester une redirection :"
+echo "On redirige la sortie de \"$ cat test.txt\" vers log.txt"
+cat test.txt > log.txt
